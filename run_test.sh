5080cb7383e840536bbdc105cb74793f6af9e8fe
nabeeluppel <nabeeluppel@gmail.com>
2021-04-10 00:21:20 +0200

    trying to add tests

--- run_test.sh
+++ run_test.sh
@@ -5,4 +5,4 @@ python src/bot.py -t &
 sleep 5s
 
 python src/tests/test_basic.py -c "$Channel_ID" --run all "$Target_ID" "$Tester"
-coverage run -m src/tests/test_basic.py -c "$Channel_ID" --run all "$Target_ID" "$Tester"
+

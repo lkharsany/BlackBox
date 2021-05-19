986ae049fe686e7352363f159207d4b6de38c22d
Nabeel Uppel <nabeeluppel@gmail>
2021-05-19 16:10:14 +0200

    small change

--- travis.sql
+++ travis.sql
@@ -1,7 +1,3 @@
-# Create Testuser
-CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
-
 # Create DB
 CREATE DATABASE IF NOT EXISTS `testDB` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `testDB`;

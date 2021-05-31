a229e1492743cc95e3cd1e98d754c5c7220ececb
neeloufah <2095284@students.wits.ac.za>
2021-06-01 01:04:42 +0200

    Bot records reactions

--- src/bot.py
+++ src/bot.py
@@ -6,6 +6,7 @@ from pretty_help import PrettyHelp
 
 bot = commands.Bot(command_prefix='./', help_command=PrettyHelp(sort_commands=True, show_index=False))
 
+''' 
 # NEED TO RUN IT WITH ARGUMENT -t for testing
 if (len(sys.argv) - 1) != 0 and sys.argv[1] == "-t":
     from distest.patches import patch_target
@@ -14,7 +15,8 @@ else:
     load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
-
+'''
+TOKEN = 'ODI3NjExMTgyNzUxOTQwNjE4.YGdi-g.InzJo2uXL_Zez9mnjL4Kys9oyL4'
 bot.load_extension("cogs.SQLCog")
 bot.load_extension("cogs.BasicCog")
 bot.load_extension("cogs.ReactionCog")

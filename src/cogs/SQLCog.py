4c552821f6aa3610a608903e794108caedc905c3
Matthew <2095294@students.wits.ac.za>
2021-06-04 00:21:53 +0200

    stats command

--- src/cogs/SQLCog.py
+++ src/cogs/SQLCog.py
@@ -651,22 +651,22 @@ class SQLCog(commands.Cog):
         conn = Db.open()
 
         serverID = ctx.guild.id
-
+        print(table)
         if isBot:
-            print(table)
+            #print(table)
             sql_q = pd.read_sql_query(
                 f'''select discord_id,discord_username from {table} where 
                    server_id = {serverID} ''',
                 conn)
             df = pd.DataFrame(sql_q)
-            df.to_csv(r'./testGstats.csv', index=False, header=True)
+            df.to_csv(r'src/cogs/testGstats.csv', index=False, header=True)
         else:
             sql_q = pd.read_sql_query(
                 f'''select discord_id,discord_username,record_count,last_message_date,record_count_20 from {table} where 
                    server_id = {serverID} ''',
                 conn)
             df = pd.DataFrame(sql_q)
-            df.to_csv(r'./Gstats.csv', index=False, header=True)
+            df.to_csv(r'src/cogs/Gstats.csv', index=False, header=True)
             await ctx.message.author.send(file=discord.File('./Gstats.csv'))
 
         await ctx.message.channel.send("General Stats file sent.")

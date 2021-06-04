2c12811933f75c34f1a5b224a645ff8e286f1c9f
Matthew <2095294@students.wits.ac.za>
2021-06-04 02:33:55 +0200

    stats

--- src/tests/test_sql.py
+++ src/tests/test_sql.py
@@ -77,7 +77,7 @@ created_channel = None
 async def test_statsSent(interface):
     await interface.send_message("Message added test")
     await interface.get_delayed_reply(1, interface.assert_message_equals, "Message Added")
-    await interface.send_message("./statss")
+    await interface.send_message("./stats")
 
     with open('src/cogs/testGstats.csv', 'r') as t1, open('src/tests/testComparison.csv', 'r') as t2:
         fileone = t1.readlines()
